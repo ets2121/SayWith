1496ed525f1985d0414d5f03d402a4dcdd6dccc4
Markjohn Valdez <valdezmarkjohn21@gmail.com>
2025-08-06 15:43:07 +0000

    Yes please create it and give me the content of the file here so that i

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,14 +7,14 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAIUgk1yaxXz1ZXmfgjbfKMVuVUU-vAEnA",
-  authDomain: "prohub-3d621.firebaseapp.com",
-  databaseURL: "https://prohub-3d621-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "prohub-3d621",
-  storageBucket: "prohub-3d621.appspot.com",
-  messagingSenderId: "43396697325",
-  appId: "1:43396697325:web:da45106718b7aa842650e4",
-  measurementId: "G-3FWFK2R49D"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
