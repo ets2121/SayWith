e811012aad1c53be065b85369f6d35cb0e67013d
Markjohn Valdez <valdezmarkjohn21@gmail.com>
2025-08-19 13:57:22 +0000

    When i forgot the credentials you can use this credentials and put this

--- src/lib/r2.ts
+++ src/lib/r2.ts
@@ -2,10 +2,11 @@
 import S3 from 'aws-sdk/clients/s3';
 
 const R2 = new S3({
-  endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  endpoint: process.env.R2_ENDPOINT,
   accessKeyId: `${process.env.R2_ACCESS_KEY_ID}`,
   secretAccessKey: `${process.env.R2_SECRET_ACCESS_KEY}`,
   signatureVersion: 'v4',
+  region: 'auto',
 });
 
 export async function getSignedUrl(filePath: string): Promise<string> {
