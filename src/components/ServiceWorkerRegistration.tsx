cbb3215105e458162af15c89ac91275d51505e2c
ets2121 <easytechsolution.ets@gmail.com>
2025-08-21 16:02:55 +0800

    Register the previous service workerUpdate ServiceWorkerRegistration.tsx

--- src/components/ServiceWorkerRegistration.tsx
+++ src/components/ServiceWorkerRegistration.tsx
@@ -6,7 +6,7 @@ import { useEffect } from 'react';
 export default function ServiceWorkerRegistration() {
   useEffect(() => {
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/custom-sw.js')
+      navigator.serviceWorker.register('/sw.js')
         .then(registration => console.log('Service Worker registered with scope:', registration.scope))
         .catch(error => console.error('Service Worker registration failed:', error));
     }
